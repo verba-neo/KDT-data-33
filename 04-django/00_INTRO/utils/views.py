4b4c084593e58abe72a2a73463762d31ab14573f
verba-neo <verba.it.corp@gmail.com>
2024-01-05 17:12:04 +0900

    django d1

--- 04-django/00_INTRO/utils/views.py
+++ 04-django/00_INTRO/utils/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 import random
 
+import requests
+from bs4 import BeautifulSoup
 
 def lotto(request):
     lotto_numbers = random.sample(range(1, 46), 6)
     lotto_numbers.sort()
-    
+
     return render(request, 'lotto.html', {
         'lotto_numbers': lotto_numbers,
     })
 
 
+
 def kospi(request):
-    return render(request, 'kospi.html')
+    URL = 'https://finance.naver.com/sise'
+    res = requests.get(URL)
+    doc = BeautifulSoup(res.text, 'html.parser')
+    kospi = doc.select_one('#KOSPI_now').text
+
+    return render(request, 'kospi.html', {
+        'kospi': kospi,
+    })
