43e98f7b5d42abc8f68106b61b13cd6d52506901
verba-neo <verba.it.corp@gmail.com>
2024-01-10 16:29:00 +0900

    crud ing

--- 04-django/02_MODEL/board/views.py
+++ 04-django/02_MODEL/board/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
+# 생성
 def new(request):
     return render(request, 'board/new.html')
 
@@ -10,9 +11,11 @@ def create(request):
     article.title = request.POST['title']
     article.content = request.POST['content']
     article.save()
-    # 사용자에게 어떤 응답을 해줄지 고민
+    # 저장 후 상세보기로 redirect
+    return redirect('board:detail', article.pk)
 
 
+# 조회
 def index(request):
     articles = Article.objects.all()
     return render(request, 'board/index.html', {
@@ -25,3 +28,26 @@ def detail(request, pk):
     return render(request, 'board/detail.html', {
         'article': article,
     })
+
+
+# 수정
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request, 'board/edit.html', {
+        'article': article,
+    })
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST['title']
+    article.content = request.POST['content']
+    article.save()
+    # 저장 후 상세보기로 redirect
+    return redirect('board:detail', article.pk)
+
+# 삭제
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('board:index')
