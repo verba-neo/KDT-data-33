b22968adbd3bc91032438e7f36680727a383434c
verba-neo <verba.it.corp@gmail.com>
2024-01-15 10:51:06 +0900

    code changing

--- 04-django/02_MODEL/board/urls.py
+++ 04-django/02_MODEL/board/urls.py
@@ -7,9 +7,7 @@ app_name = 'board'
 
 urlpatterns = [
 # Create
-    # board/new/ => 게시글 작성용 <form> 제공 => 'board:new'
-    path('new/', views.new, name='new'),
-    # board/create/ => 게시글 DB에 저장
+    # board/create/ => HTML / DB에 저장
     path('create/', views.create, name='create'),
 # Read
     # board/ => 전체 게시글 목록 조회
@@ -17,8 +15,6 @@ urlpatterns = [
     # board/1/ => 특정(pk)게시글 상세 조회 => 'board:detail'
     path('<int:pk>/', views.detail, name='detail'),
 # Update
-    # board/1/edit/ => 게시글 수정용 <form> 제공
-    path('<int:pk>/edit/', views.edit, name='edit'),
     # board/1/update/ => 수정된 게시글을 DB에 저장
     path('<int:pk>/update/', views.update, name='update'),
 # Delete
--- 04-django/02_MODEL/board/views.py
+++ 04-django/02_MODEL/board/views.py
@@ -3,28 +3,17 @@ from .models import Article
 from .forms import ArticleForm
 
 # 생성
-def new(request):
-    form = ArticleForm()
-    return render(request, 'board/new.html', {
-        'form': form,
-    })
-
-
 def create(request):
-    # 넘어온 데이터를 통째로 form 에 입력
-    form = ArticleForm(request.POST)
-    # form 이 유효하다면,
-    if form.is_valid():
-        # form을 통해 article instance 저장
-        article = form.save()
-        # 저장 후 상세보기로 redirect
-        return redirect('board:detail', article.pk)
-    # 유효하지 않다면
-    else:
-        # 사용자 입력 데이터 + 에러 메시지를 다시 new.html 에서 보여줌
-        return render(request, 'board/new.html', {
-            'form': form,
-        })
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('board:detail', article.pk)
+        
+    elif request.method == 'GET':
+        form = ArticleForm()
+
+    return render(request, 'board/new.html', {'form': form })
 
 
 # 조회
@@ -43,27 +32,21 @@ def detail(request, pk):
 
 
 # 수정
-def edit(request, pk):
+def update(request, pk):
     article = Article.objects.get(pk=pk)
-    form = ArticleForm(instance=article)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save()
+            return redirect('board:detail', article.pk)
+    
+    elif request.method == 'GET':
+        form = ArticleForm(instance=article)
+
     return render(request, 'board/edit.html', {
-        'article': article,
         'form': form,
     })
 
-
-def update(request, pk):
-    article = Article.objects.get(pk=pk)
-    # 사용자 데이터와 기존 article 을 같이 활용해야함!
-    form = ArticleForm(request.POST, instance=article)
-    if form.is_valid():
-        article = form.save()
-        return redirect('board:detail', article.pk)
-    else:
-        return render(request, 'board/edit.html', {
-            'article': article,
-            'form': form,
-        })
 
 # 삭제
 def delete(request, pk):
