b0504814df70ef669e6c7e5c5b89b847ed98532e
verba-neo <verba.it.corp@gmail.com>
2024-01-17 15:47:02 +0900

    Login, Signup

---
+++ 04-django/RESTART/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    # accounts/signup/
+    path('signup/', views.signup, name='signup'),
+    # accounts/login/
+    path('login/', views.login, name='login'),
+    # accounts/logout/
+    path('logout/', views.logout, name='logout'),
+]
--- 04-django/RESTART/accounts/views.py
+++ 04-django/RESTART/accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
-# Create your views here.
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # 회원 등록
+            user = form.save()
+            # 바로 이어서 로그인 시켜주기
+            auth_login(request, user)
+            return redirect('board:article_index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+
+
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.method == 'POST':
+        # AuthenticationForm 은 다른 ModelForm 들과는 다르다! 
+        form = AuthenticationForm(request, data=request.POST)
+        # id/pw 가 맞다면
+        if form.is_valid():
+            # 해당 사용자를 조회해서 return
+            user = form.get_user()
+            # 로그인 시켜주기
+            auth_login(request, user)
+            return redirect('board:article_index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {
+        'form': form,
+    })
+
+
+def logout(request):
+    pass
