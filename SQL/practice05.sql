1e7ecf6b20ac9c20e0ad8fbdb3359f0dd440890d
verba-neo <verba.it.corp@gmail.com>
2024-01-19 15:12:46 +0900

    sql

--- SQL/practice05.sql
+++ SQL/practice05.sql
@@ -1,2 +1,13 @@
 -- practice05.sql
 
+USE pet_shop;
+
+UPDATE cats SET name='Jack' WHERE name='Jackson';
+UPDATE cats SET breed='British Shorthair'
+WHERE name='Ringo';
+UPDATE cats SET age=12 WHERE breed='Maine Coon';
+
+
+DELETE FROM cats WHERE age=4;
+DELETE FROM cats WHERE cat_id=age;
+DELETE FROM cats;
